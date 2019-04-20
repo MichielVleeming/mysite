b41cd15316d5dcb2884b01396a84038ba6bfb507
michiel <Michiel.vleeming@gmail.com>
2019-04-20 14:20:35 +0200

    Changed DB

--- moistureApp/admin.py
+++ moistureApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import MoistureMeter, Arduino
+
+admin.site.register(MoistureMeter)
+admin.site.register(Arduino)
--- moistureApp/models.py
+++ moistureApp/models.py
@@ -1,10 +1,21 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 from django.db import models
+from django.utils import timezone
+
+
+
+class Arduino(models.Model):
+    arduino_name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.arduino_name
 
 
 class MoistureMeter(models.Model):
+    arduino = models.ForeignKey(Arduino, on_delete=models.CASCADE)
     moisture_measurement = models.IntegerField(default=0)
-    arduino_name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
